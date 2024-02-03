aa476d74c7807c36381bb780cb494e1f91b78bc6
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-02-04 00:31:08 +0900

    [닷지볼/카메라/야준서] 카메라 설정

--- Dodgeball/Source/Dodgeball/DodgeballCharacter.cpp
+++ Dodgeball/Source/Dodgeball/DodgeballCharacter.cpp
@@ -42,9 +42,13 @@ ADodgeballCharacter::ADodgeballCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
-
+	CameraBoom->TargetArmLength = 900.0f; // The camera follows at this distance behind the character	
+	CameraBoom->bUsePawnControlRotation = false; // Rotate the arm based on the controller
+	CameraBoom->bInheritPitch = false;
+	CameraBoom->bInheritYaw = false;
+	CameraBoom->bInheritRoll = false;
+	CameraBoom->SetRelativeRotation(FRotator(-70.f, 0.f, 0.f));
+	
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
@@ -69,6 +73,10 @@ void ADodgeballCharacter::BeginPlay()
 	}
 }
 
+void ADodgeballCharacter::LookAtActor(AActor* TargetActor)
+{
+}
+
 //////////////////////////////////////////////////////////////////////////
 // Input
 
@@ -77,15 +85,15 @@ void ADodgeballCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInput
 	// Set up action bindings
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent)) {
 		
-		// Jumping
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
-		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+		// // Jumping
+		// EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		// EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
 
 		// Moving
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ADodgeballCharacter::Move);
 
-		// Looking
-		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ADodgeballCharacter::Look);
+		// // Looking
+		// EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ADodgeballCharacter::Look);
 	}
 	else
 	{
