977eb89cd175125bf74b1d249d6caeeaf70fe118
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-02-13 23:05:27 +0900

    [닷지볼/닷지볼/야준서] 닷지 볼 생성

---
+++ Dodgeball/Source/Dodgeball/DodgeballProjectile.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DodgeballProjectile.h"
+
+#include "DodgeballCharacter.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+ADodgeballProjectile::ADodgeballProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	SphereComponent->SetSphereRadius(35.f);
+	SphereComponent->SetCollisionProfileName(FName("Dodgeball"));
+	SphereComponent->SetSimulatePhysics(true);
+	SphereComponent->SetNotifyRigidBodyCollision(true);
+	SphereComponent->OnComponentHit.AddDynamic(this, &ADodgeballProjectile::OnHit);
+	RootComponent = SphereComponent;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->InitialSpeed = 1500.f;
+}
+
+// Called when the game starts or when spawned
+void ADodgeballProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADodgeballProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ADodgeballProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	if(Cast<ADodgeballCharacter>(OtherActor) != nullptr)
+	{
+		Destroy();
+	}
+}
+
---
+++ Dodgeball/Source/Dodgeball/DodgeballProjectile.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "DodgeballProjectile.generated.h"
+
+UCLASS()
+class DODGEBALL_API ADodgeballProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADodgeballProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Dodgeball, meta = (AllowPrivateAccess = "true"))
+	class USphereComponent* SphereComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Dodgeball, meta = (AllowPrivateAccess = "true"))
+	class UProjectileMovementComponent* ProjectileMovement;
+	
+public:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+};
