4002015c721db19e166730bfa0746943c43a4369
YAJUNSEO\yajunseo <yajunseo313@naver.com>
2024-02-22 23:56:20 +0900

    [닷지볼/닷지볼/야준서] 리스타트 기능 구현

--- Dodgeball/Source/Dodgeball/Dodgeball.Build.cs
+++ Dodgeball/Source/Dodgeball/Dodgeball.Build.cs
@@ -8,6 +8,10 @@ public class Dodgeball : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+			"UMG", "Slate", "SlateCore"
+		});
 	}
 }
---
+++ Dodgeball/Source/Dodgeball/RestartWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "RestartWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DODGEBALL_API URestartWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* RestartButton;
+
+public:
+	virtual void NativeOnInitialized() override;
+	
+protected:
+	UFUNCTION()
+	void OnRestartClicked();
+};
